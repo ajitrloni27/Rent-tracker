6143552cd7d388c157c0ab2dcac2781e6df8e354
ajitrloni27 <ajitrloni27@gmail.com>
2026-08-22 19:15:18 +0530

    Update theme

--- lib/core/theme.dart
+++ lib/core/theme.dart
@@ -20,7 +20,7 @@ class AppTheme {
         elevation: 0,
         centerTitle: true,
       ),
-      cardTheme: CardTheme(
+      cardTheme: CardThemeData(
         elevation: 2,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(16),
@@ -45,7 +45,7 @@ class AppTheme {
         elevation: 0,
         centerTitle: true,
       ),
-      cardTheme: CardTheme(
+      cardTheme: CardThemeData(
         elevation: 2,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(16),
